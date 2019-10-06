20ad2cded9a90fc5adbdc4a8f3057dc9db2f8c1f
frotal <edgar.froot@gmail.com>
2019-10-06 16:51:56 -0700

    Documentada: vector_grafico.py

--- vector_grafico.py
+++ vector_grafico.py
@@ -1,11 +1,21 @@
 import pygame
 
-
 class VectorGrafico:
+	"""Representación gráfica en una superficie de un Vector.
+	La principal función de esta clase es dibujar un vector dado, tomando
+	un punto como origen.
+
+	Args:
+		origen (Punto): Un punto en la superficie
+		vector (Vector): Un vector en la superficie
+
+	Attributes:
+		origen (:class: Punto): Un punto en la superficie
+		vector (:class: Vector): Un vector en la superficie
+
+	"""
 
 	def __init__(self, origen, vector):
-		#origen: Punto
-		#vector: Vector
 		self._origen = origen
 		self._vector = vector
 
@@ -18,6 +28,14 @@ class VectorGrafico:
 		return self._vector
 
 	def render(self, **args):
+		"""Renderiza el vector desde el origen hasta la punta del vector
+
+		Args:
+			**args: Un conjunto de argumentos opcionales (excepto superficie):
+				color ((int, int, int))
+				grosor (int)
+				superficie (pygame.Surface): La superficie donde se renderizará.
+		"""
 		if 'color' in args:
 			color = args['color']
 		if 'grosor' in args:
