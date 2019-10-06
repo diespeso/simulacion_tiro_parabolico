2f73714c117773cc6731cb86c317487efa6b9f8a
frotal <edgar.froot@gmail.com>
2019-10-06 16:45:12 -0700

    Documentada: variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
 """
-modulo de variables del sistema
+Este módulo contiene a las variables que se usan en el sistema.
+Su principal utilidad es poder editar estos valores más fácilmente
+sólo editando los valores en este archivo.
 """
 
 SIMULACION_FONDO_COLOR = (255, 255, 255)
@@ -12,6 +17,8 @@ PARTICULA_VECTOR_COLOR = (87, 35, 100)
 PARTICULA_VECTOR_X_COLOR = (0, 255, 0)
 PARTICULA_VECTOR_Y_COLOR = (0, 0, 255)
 PARTICULA_VECTOR_X_GROSOR = PARTICULA_VECTOR_Y_GROSOR = 2
+
+PARTICULA_MOVIL_COLOR = (255, 0, 255)
 
 TRAYECTORIA_COLOR = (150, 255, 255)
 TRAYECTORIA_GROSOR = 1
